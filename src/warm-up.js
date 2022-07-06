50b93023f60f9745c1fa827eb2e6479986bfe9de
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-06 17:03:47 -0500

    Initial commit

--- src/warm-up.js
+++ src/warm-up.js
@@ -13,26 +13,40 @@
 // var type = "Dessert";
 // var inStock = 10
 
+var food = "Pizza";
+var type = "Meal";
+var inStock = 3
 
 
-// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
+// Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make
 // the inStock count double what it currently is.
 
+inStock = 3 * 2
+console.log(inStock);
 
-// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
+// Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack!
 // Use a built in math operator to decrement the inStock count by 1.
 
+inStock = 6 - 1
+console.log(inStock);
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
 
-
+var isFavorite = false;
 
 // Create a new variable named "shouldOrderMore", and use a comparison operator to
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
+function shouldOrderMore() {
+  if () {
+
+  } else {
+    
+  }
+}
 
 
 //------------------------- Conditionals -------------------------//
@@ -40,7 +54,7 @@
 
 
 // Express the following in code: If the variable "stockCount" is greater or equal to 1, log
-// the statement 'I'm going to eat some [food]!', otherwise, log the statement 
+// the statement 'I'm going to eat some [food]!', otherwise, log the statement
 // 'We need to get some more [food].'
 
 
