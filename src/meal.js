23fae9b079686bf5ee6add7893a103dc5f6f543b
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-07 22:53:58 -0500

    Start Iteration 0

--- src/meal.js
+++ src/meal.js
@@ -3,11 +3,29 @@ function nameMenuItem(name) {
   return `Delicious ${name}`
 }
 
+function createMenuItem(name, price, type) {
+  var menuItemName = name;
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem
+}
+
+function addIngredients(ingredientsToAdd, ingredients) {
+  ingredients[0] = "cheese";
+  //for (var i = 0; i < ingredients.length; i++) {
+    //if (ingredients[i] <=3) {
+      return ingredients
+    //}
+  //}
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
