35bf6dc0df7db1539d09cd530b8139334d109798
Dana Chapman <dana.leigh.dlc@gmail.com>
2022-07-09 15:55:46 -0500

    Completed order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(orderType, deliveryOrders) {
+  // add new orders to an order type list
+    if (deliveryOrders.length < 3) {
+      deliveryOrders.push(orderType)
+  }
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  // remove an order from the array by order#
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber) {
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+function listItems(deliveryOrders) {
+  // want to PUSH the item into the items array
+  var items = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items.push(deliveryOrders[i].item)
+  }
+  return items.join(', ')
+}
+
+function searchOrder(deliveryOrders, itemName) {
+  // should eval true if an order is listed
+  // deliveryOrders has 2 elements
+  var result = false;
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === itemName) {
+      result = true;
+    }
+  }
+  return result
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
